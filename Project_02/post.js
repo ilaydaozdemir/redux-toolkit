e7ee18616b110fb56828d534ef661541f735b6c8
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-08-30 23:43:19 +0300

    Add Switch Statement

--- Project_02/post.js
+++ Project_02/post.js
@@ -32,17 +32,20 @@ const removePostAction = (id) => {
 
 //reducer
 const postReducer = (state = initialState, action) => {
-  if (action.type === "ADD_POST") {
-    return {
-      posts: [...state.posts, action.payload],
-    };
-  } else if (action.type === "REMOVE_POST") {
-    return {
-      posts: state.posts.filter((post) => {
-        return post.id !== action.id;
-      }),
-    };
-  } else return state;
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        posts: [...state.posts, action.payload],
+      };
+    case REMOVE_POST:
+      return {
+        posts: state.posts.filter((post) => {
+          return post.id !== action.id;
+        }),
+      };
+    default:
+      return state;
+  }
 };
 //store
 const store = createStore(postReducer);
