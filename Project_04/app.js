a680cff5f38a1f135c505a88ecfad6f608092e92
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-09-07 17:18:53 +0300

    Add Redux Toolkit Store Setup

--- Project_04/app.js
+++ Project_04/app.js
@@ -1,4 +1,9 @@
-const { createAction, nanoid, createReducer } = require("@reduxjs/toolkit");
+const {
+  createAction,
+  nanoid,
+  createReducer,
+  configureStore,
+} = require("@reduxjs/toolkit");
 
 //initial state
 const initialState = {
@@ -17,13 +22,13 @@ const incrementBy = createAction("INCREMENT_BY", (amount, user) => {
     },
   };
 });
-console.log(incrementBy(20, "ilayda"));
+
 //reducer
 //1.builder callback notation
 //2.map object notation
 
 //1.builder callback notation
-const counterSlice2 = createReducer(initialState, (builder) => {
+const counterSlice = createReducer(initialState, (builder) => {
   //for increment
   builder.addCase(increment, (state) => {
     state.count += 1;
@@ -42,19 +47,13 @@ const counterSlice2 = createReducer(initialState, (builder) => {
   });
 });
 
-//2.map object notation
-const counterSlice = createAction(initialState, {
-  [increment]: (state) => {
-    state.counter += 1;
-  },
-  [decrement]: (state) => {
-    state.counter -= 1;
-  },
-  [resetCounter]: (state) => {
-    state.counter = 0;
-  },
-  [resetCounter]: (state, action) => {
-    state.counter += action.payload.amount;
-  },
-});
 //store
+const store = configureStore({
+  reducer: counterSlice,
+});
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(incrementBy(55));
+console.log(store.getState());
