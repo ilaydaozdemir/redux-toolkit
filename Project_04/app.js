589999a69d17d3041742be5ae35fdab9db27f5c4
ozdemirilayda <ozdemirilayda95@gmail.com>
2023-09-07 16:58:07 +0300

    Add Builder Notation-CreateReducer

--- Project_04/app.js
+++ Project_04/app.js
@@ -1,4 +1,4 @@
-const { createAction, nanoid } = require("@reduxjs/toolkit");
+const { createAction, nanoid, createReducer } = require("@reduxjs/toolkit");
 
 //initial state
 const initialState = {
@@ -14,11 +14,29 @@ const incrementBy = createAction("INCREMENT_BY", (amount, user) => {
   return {
     payload: {
       amount,
-      user,
-      id: nanoid(),
     },
   };
 });
 console.log(incrementBy(20, "ilayda"));
 //reducer
+//1.builder callback notation
+//2.map object notation
+createReducer(initialState, (builder) => {
+  //for increment
+  builder.addCase(increment, (state) => {
+    state.count += 1;
+  });
+  //for decrement
+  builder.addCase(decrement, (state) => {
+    state.count -= 1;
+  });
+  //for resetCounter
+  builder.addCase(resetCounter, (state) => {
+    state.count = 0;
+  });
+  //for incrementBy
+  builder.addCase(incrementBy, (state, action) => {
+    state.count += action.payload.amount;
+  });
+});
 //store
